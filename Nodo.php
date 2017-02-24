acb4ca90e4d2797ec0c9fddab22406635bc7e500
Daniel Vergara <danielvsuarez01@gmail.com>
2017-02-24 10:35:32 -0500

    Se modificó clase nodo para que fuera una lista de empleados

--- Nodo.php
+++ Nodo.php
@@ -1,17 +1,41 @@
 <?php
   class Nodo{
-    private $Info;
+    private $Codigo;
+    private $Nombre;
+    private $Edad;
+    private $Salario;
     private $Sig;
     //construct
-    function __construct($Info){
-      $this->Info = $Info;
+    function __construct($Codigo, $Nombre, $Edad, $Salario){
+      $this->Codigo = $Codigo;
+      $this->Nombre = $Nombre;
+      $this->Edad = $Edad;
+      $this->Salario = $Salario;
     }
     //getters and setters
-    function getInfo(){
-      return $this->Info;
+    function getCodigo(){
+      return $this->Codigo;
     }
-    function setInfo($Info){
-      $this->Info = $Info;
+    function setCodigo($Codigo){
+      $this->Codigo = $Codigo;
+    }
+    function getNombre(){
+      return $this->Nombre;
+    }
+    function setNombre($Nombre){
+      $this->Nombre = $Nombre;
+    }
+    function getEdad(){
+      return $this->Edad;
+    }
+    function setEdad($Edad){
+      $this->Edad = $Edad;
+    }
+    function getSalario(){
+      return $this->Salario;
+    }
+    function setSalario($Salario){
+      $this->Salario = $Salario;
     }
     function getSig(){
       return $this->Sig;
