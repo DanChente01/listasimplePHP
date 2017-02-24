a63d5cb11236ba65fda56413842eb33722870679
Daniel Vergara <danielvsuarez01@gmail.com>
2017-02-24 12:42:32 -0500

    Se modificó la prueba para la lista empleados

--- index.php
+++ index.php
@@ -8,33 +8,40 @@
     <h1>Listas Simples con PHP</h1>
     <?php
       include 'ListaSimple.php';
-      $nuevaLista = new ListaSimple();
-      $elemento0 = new Nodo("1");
-      $elemento1 = new Nodo("A");
-      $elemento2 = new Nodo("B");
-      $elemento3 = new Nodo("C");
+      $ListaTrabajadores = new ListaSimple();
+      $Trabajador1 = new Nodo(01,"Daniel",20,15000);
+      $Trabajador2 = new Nodo(02,"Robinson",19,20000);
+      $Trabajador3 = new Nodo(03,"Ana",18,10000);
+      $Trabajador4 = new Nodo(04,"Karen",18,13000);
 
-      $nuevaLista->AgregarNodoPrincipio($elemento0);
-      $nuevaLista->AgregarNodoPrincipio($elemento1);
-      $nuevaLista->AgregarNodoPrincipio($elemento2);
-      $nuevaLista->AgregarNodoPrincipio($elemento3);
+      $ListaTrabajadores->AgregarNodoPrincipio($Trabajador1);
+      $ListaTrabajadores->AgregarNodoPrincipio($Trabajador2);
+      $ListaTrabajadores->AgregarNodoPrincipio($Trabajador3);
+      $ListaTrabajadores->AgregarNodoPrincipio($Trabajador4);
 
-      $mensaje = $nuevaLista->VisualizarLista();
+      $mensaje = $ListaTrabajadores->VisualizarLista();
       echo "<br><br>".$mensaje;
-      $buscar = $nuevaLista->BuscarNodo("o");
+
+      $buscar = $ListaTrabajadores->BuscarNodo("01");
       if ($buscar == true) {
-          echo "<br><b>Buscar Nodo: </b>".$buscar->getInfo();
+          echo "<br><b>Buscar Nodo: </b>".$buscar->getCodigo().$buscar->getNombre();
       }else {
         echo "<br><b>Buscar Nodo:</b> Elemento no existe";
       }
-      $eliminar = $nuevaLista->EliminarNodo("A");
+      $eliminar = $ListaTrabajadores->EliminarNodo("02");
       if ($eliminar== true) {
         echo "<br><b>Eliminar: </b> Elemento eliminado";
       }else {
         echo "<br><b>Eliminar: </b> Elemento no existe";
       }
-      $mensaje = $nuevaLista->VisualizarLista();
+      $mensaje = $ListaTrabajadores->VisualizarLista();
       echo "<br><br>".$mensaje;
-     ?>
+      echo "<br>";
+      $promedio = $ListaTrabajadores->masQuePromedio();
+      echo "Mas que el promedio: ".$promedio;
+      echo "<br>";
+      $suma = $ListaTrabajadores->sumaSalarios();
+      echo "Suma Salarios:".$suma;
+    ?>
   </body>
 </html>
